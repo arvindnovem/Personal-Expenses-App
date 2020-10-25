6253af75c491b596ec30cc3f097f9217788b581b
Arvind S <arvindnovem@gmail.com>
2020-10-25 11:18:21 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -51,10 +51,10 @@ class MyHomePage extends StatelessWidget {
                 Container(
                     margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
                     decoration: BoxDecoration(
-                        border: Border.all(color: Colors.purple, width: 5)),
+                        border: Border.all(color: Colors.purple, width: 2)),
                     padding: EdgeInsets.all(10),
                     child: Text(
-                      tx.amount.toString(),
+                      '\$${tx.amount}', //String interpolation
                       style: TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 20,
