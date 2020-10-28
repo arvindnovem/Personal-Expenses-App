ea25809366a15da0c94090d9f4a7831570d1d611
Arvind S <arvindnovem@gmail.com>
2020-10-28 23:22:03 +0530

    Added ListView

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -7,32 +7,37 @@ class TransactionList extends StatelessWidget {
   TransactionList(this.transactions);
   @override
   Widget build(BuildContext context) {
-    return Column(
-        children: transactions.map((tx) {
-      return Card(
-          child: Row(
-        children: [
-          Container(
-              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-              decoration: BoxDecoration(
-                  border: Border.all(color: Colors.purple, width: 2)),
-              padding: EdgeInsets.all(10),
-              child: Text(
-                '\$${tx.amount}', //String interpolation
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.purple),
-              )),
-          Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            Text(tx.title,
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-            Text(DateFormat().format(tx.date),
-                style: TextStyle(
-                    fontWeight: FontWeight.bold, color: Colors.black)),
-          ]),
-        ],
-      ));
-    }).toList());
+    return Container(
+        height: 300,
+        child: ListView.builder(
+          itemBuilder: (ctx, index) {
+            return Card(
+                child: Row(
+              children: [
+                Container(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.purple, width: 2)),
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      '\$${transactions[index].amount}', //String interpolation
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple),
+                    )),
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                  Text(transactions[index].title,
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+                  Text(DateFormat().format(transactions[index].date),
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.black)),
+                ]),
+              ],
+            ));
+          },
+          itemCount: transactions.length,
+        ));
   }
 }
