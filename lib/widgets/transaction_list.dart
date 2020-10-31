2b3d2ed9c343b00a6be77c27b8df48d4cb811ddf
Arvind S <arvindnovem@gmail.com>
2020-10-31 19:35:36 +0530

    added fonts folder under assets for saving fonts

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -12,29 +12,33 @@ class TransactionList extends StatelessWidget {
         child: ListView.builder(
           itemBuilder: (ctx, index) {
             return Card(
-                child: Row(children: [
-              Container(
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.purple, width: 2)),
-                  padding: EdgeInsets.all(10),
-                  child: Text(
+                child: Row(
+              children: [
+                Container(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.purple, width: 2)),
+                    padding: EdgeInsets.all(10),
+                    child: Text(
                       '\$${transactions[index].amount.toStringAsFixed(2)}', //String interpolation
                       style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Theme.of(context).primaryColor,
-                      ))),
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                Text(transactions[index].title,
-                    style:
-                        TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-                Text(DateFormat().format(transactions[index].date),
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Theme.of(context).primaryColor),
+                    )),
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                  Text(transactions[index].title,
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+                  Text(
+                    DateFormat().format(transactions[index].date),
                     style: TextStyle(
                         fontWeight: FontWeight.bold,
-                        color: Theme.of(context).primaryColor)),
-              ]),
-            ]));
+                        color: Theme.of(context).primaryColor),
+                  )
+                ]),
+              ],
+            ));
           },
           itemCount: transactions.length,
         ));
